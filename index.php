d5e50016c86d00c831032a37bf0fdf26bea3c6ab
federicosuarez7 <federico.saboretto@gmail.com>
2023-12-03 13:47:26 -0300

    Instalamos Xampp e iniciamos el primer archivo php (PHP)

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendiendo PHP</title>
+</head>
+<body>
+    <?php 
+        print "Bienvenidos al curso de PHP <br>";
+        print "Hola Mundo <br>";
+        
+        
+        
+        
+        
+        
+    ?>
+</body>
+</html>
