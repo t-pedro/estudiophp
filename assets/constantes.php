e1cff1b070965407cb7f01724d7acc8968efeba5
federicosuarez7 <federico.saboretto@gmail.com>
2023-12-08 11:11:00 -0300

    Constantes definidas y predefinidas (PHP)

---
+++ assets/constantes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constantes</title>
+</head>
+<body>
+    <?php
+        define("AUTOR","Federico");
+        echo "El autor es:  " . AUTOR;
+
+
+
+    ?>
+</body>
+</html>
