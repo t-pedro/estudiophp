1c0973ae717739c06945b306a28a628da76a2547
federicosuarez7 <federico.saboretto@gmail.com>
2023-12-13 18:01:12 -0300

    Funciones y casting de datos (PHP)

---
+++ CalculadoraPHP/funciones.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funciones</title>
+</head>
+<body>
+    <?php
+        $num1= rand(10,66);
+        echo "El numero aleatorio es: $num1";
+        //Casting de datos
+        $num2=1.2;
+        $resultado=(int)$num2;
+        echo "<br>El resultado es: $resultado";
+    ?>
+</body>
+</html>
