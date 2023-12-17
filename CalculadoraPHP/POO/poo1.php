4339f190e2ff07c833677409dbd48124c7ba6f45
federicosuarez7 <federico.saboretto@gmail.com>
2023-12-17 11:43:51 -0300

    POO, creación de clases e instancias de clase (PHP)

---
+++ CalculadoraPHP/POO/poo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progrmacion orientada a objetos</title>
+</head>
+<body>
+    <?php
+        //Creacion de clase
+        class Coche{
+
+        }
+        //Instancia de clase
+        $renault = new Coche();
+        $mazda = new Coche(); 
+
+    ?>
+</body>
+</html>
