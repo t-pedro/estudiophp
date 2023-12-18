95bbff849d2225039d4b5568c7fbb502f36c79fb
federicosuarez7 <federico.saboretto@gmail.com>
2023-12-18 11:06:21 -0300

    POO,creacion de metodos de clase y constructores (PHP)

--- CalculadoraPHP/POO/poo1.php
+++ CalculadoraPHP/POO/poo1.php
@@ -9,11 +9,38 @@
     <?php
         //Creacion de clase
         class Coche{
+            //Propiedades a cada objeto de tipo coche
+            private int $ruedas;
+            private string $color;
+            private int $motor;
+            private int $puertas;
 
+            //Constructor de clase
+            public function __construct (){
+                $this -> ruedas=4;
+                $this -> color ="";
+                $this -> motor = 1600;
+                $this -> puertas = 5;
+            }
+
+            //Métodos
+            function arrancar(){
+                echo "Arrancó el vehículo";
+            }
+            function girar(){
+                echo "El vehículo esta doblando";
+            }
+            function frenar(){
+                echo "El vehículo está frenando";
+            }
         }
         //Instancia de clase
         $renault = new Coche();
         $mazda = new Coche(); 
+
+        $mazda -> arrancar();
+        echo "<br>";
+        echo $mazda->ruedas;
 
     ?>
 </body>
